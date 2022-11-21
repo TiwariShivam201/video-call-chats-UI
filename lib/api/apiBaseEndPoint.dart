b19273a0936cacaaaeaaaceb147862bb7b070dcd
Shivam <s1032170105@gmail.com>
2022-11-21 12:38:41 +0530

    issue fixed

--- lib/api/apiBaseEndPoint.dart
+++ lib/api/apiBaseEndPoint.dart
@@ -1,6 +1,5 @@
 class APIEndPoint {
-  static String baseUrl =
-      'https://4w01sz4ri1.execute-api.ap-south-1.amazonaws.com/test';
+  static String baseUrl = 'BASE-URL';
 
   static String agoraToken = "client/agora-token";
 }
